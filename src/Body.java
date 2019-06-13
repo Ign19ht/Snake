7ac9f21002a798567ad9340bbef71742ef960c15
Anikin Kirill <ky.anikin@mail.ru>
2019-06-13 13:03:52 +0300

    Комментарии к Body.java
    К примеру можно сделать так. То есть я добавил комментарии к классу, теперь для других разработчиков будет более понятно что к чему

--- src/Body.java
+++ src/Body.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+* Класс нужен для создания тела обьекта (Пример: {@link Snake})
+* img - картинка самого тела
+* x - координата по оси OX
+* y - координата по оси OY
+*/
+
 public class Body {
+    
+    // картинка самого тела
     Image img = new ImageIcon("res\\body.png").getImage();
 
+    // координата по оси OX
     int x;
+    // y - координата по оси OY
     int y;
+    
     myDeque memory = new myDeque();
 
     public Body(int x, int y) {
